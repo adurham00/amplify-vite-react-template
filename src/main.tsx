8c51e27c1342ae25ac909e405e2145b4461bd218
adurham00 <apreece00@gmail.com>
2025-07-02 20:39:32 +0000

    testing for phone

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import "./index.css";
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
 Amplify.configure(outputs);
 
+ReactDOM.createRoot(document.getElementById("root")!).render(
+      <App />
+);
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,18 +5,22 @@ import './HomePage.css'; // This line imports your CSS file
 
 function HomePage() {
   return (
-    <div>
-      <h1>DurhamDesigns</h1>
-      <p>This is the landing page for your Amplify-powered application.</p>
-      <nav>
-        <ul style={{ listStyle: 'none', padding: 0, display: 'flex', gap: '15px' }}>
-          <li><Link to="/todos">Go to Todos</Link></li>
-          <li><Link to="/about">Learn About Us</Link></li>
-          {/* You can add more navigation links here as you create new pages */}
-        </ul>
-      </nav>
-    </div>
-  );
-}
+
+      <div className="header-content">
+        <div className="nav-wrapper">
+        <nav>
+          <ul className="main-nav-list">
+            <li><Link to="/todos">Home</Link></li>
+            <li><Link to="/todos">About</Link></li>
+            <li><Link to="/todos">Socials</Link></li>
+            <li><Link to="/about">Resume</Link></li>
+            {/* You can add more navigation links here as you create new pages */}
+          </ul>
+        </nav>
+        </div>
+        <h1>DurhamDesigns</h1>
+      </div>
+    );
+  }
 
 export default HomePage;
