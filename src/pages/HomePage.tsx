60d2e24e3d7dde366b329644e56a6d902a5d04cf
adurham00 <apreece00@gmail.com>
2025-07-02 22:45:10 +0000

    updated css (review)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,39 +1,65 @@
 // src/pages/HomePage.tsx
-import { Link } from 'react-router-dom'; // Import Link for navigation
-import './HomePage.css'; // This line imports your CSS file
+import { Link } from 'react-router-dom';
+import './HomePage.css';
 import { useEffect, useState } from 'react';
 import ImageCarousel from './ImageCarousel';
 import './ImageCarousel.css';
 
 function HomePage() {
+  const portfolioItems = [
+    {
+      id: 1,
+      title: 'ABIGAIL DURHAM',
+      subtitle: 'ABIGAIL DURHAM',
+      description: 'Lorem ipsum dolor sit amet... (etc)',
+    },
+    {
+      id: 2,
+      title: 'ABIGAIL DURHAM',
+      subtitle: 'ABIGAIL DURHAM',
+      description: 'Second item description text...',
+    },
+    {
+      id: 3,
+      title: 'ABIGAIL DURHAM',
+      subtitle: 'ABIGAIL DURHAM',
+      description: 'Third item description text...',
+    },
+  ];
+
   return (
     <div className="HomePage-container">
-        <div className="nav-wrapper">
-        <nav>
-          <ul className="main-nav-list">
-            <li><Link to="/todos">Home</Link></li>
-            <li><Link to="/todos">About</Link></li>
-            <li><Link to="/todos">Socials</Link></li>
-            <li><Link to="/about">Resume</Link></li>
-            {/* You can add more navigation links here as you create new pages */}
-          </ul>
-         </nav>
-       </div>
+      
+      {/* Title Header */}
+      <header className="TitleHeader">
+        <h1>ABIGAIL<br />DURHAM</h1>
+      </header>
 
+      {/* Navigation */}
+      <nav className="nav-wrapper">
+        <ul className="main-nav-list">
+          <li><Link to="#">About</Link></li>
+          <li><Link to="#">Social</Link></li>
+          <li><Link to="#">Gallery</Link></li>
+        </ul>
+      </nav>
 
-<div className="TitleHeader">
-  <h1>Abigail Durham</h1>
-</div>
+      {/* Portfolio Grid */}
+      <section className="portfolio">
+        {portfolioItems.map((item, index) => (
+          <div key={item.id} className={`portfolio-row ${index % 2 !== 0 ? 'reverse' : ''}`}>
+            <div className="image-box"></div>
+            <div className="text-box">
+              <h2>{item.title}</h2>
+              <h3>{item.subtitle}</h3>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </section>
 
-
-<ImageCarousel/>
-
-
-
-</div>   
-
-
-    );
-  }
+    </div>
+  );
+}
 
 export default HomePage;
