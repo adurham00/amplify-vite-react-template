ebc47fcc23b1ea40ead09eb85d65ce5e6424c3e8
adurham00 <apreece00@gmail.com>
2025-07-02 16:04:37 +0000

    updated user sign in auth

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const { signOut } = useAuthenticator();
+  const {user, signOut } = useAuthenticator();
   
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -23,10 +23,12 @@ function App() {
     client.models.Todo.delete({ id })
   }
   
+  
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <ul>
         {todos.map((todo) => (
           <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
