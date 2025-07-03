4caa7ae1d1351763393eb51da42dee9a67d932cf
adurham00 <apreece00@gmail.com>
2025-07-03 23:24:08 +0000

    test deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import "./index.css";
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import outputs from "./amplify_outputs.json";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
       <App />
