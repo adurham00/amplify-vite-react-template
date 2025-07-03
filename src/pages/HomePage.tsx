9b6a8e6ad89215bb6cfee86fd25318f9a8818fa0
adurham00 <apreece00@gmail.com>
2025-07-03 22:54:36 +0000

    updated fonts

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 // src/pages/HomePage.tsx
 import { Link } from 'react-router-dom';
 import './HomePage.css';
-/* import FlipCard from '../resources/FlipCard'; */
+import FlipCard from '../resources/FlipCard'; 
 
 
 function HomePage() {
@@ -10,23 +10,24 @@ function HomePage() {
   <div className="HomePage-container">
   <div className="header-nav-group">
     <header className="TitleHeader">
-      <h1>ABIGAIL<br />DURHAM</h1>
+      <h1 className="spaced-header1">ABIGAIL</h1>
+      <h1 className="spaced-header2">DURHAM</h1>
     </header>
 
     <nav className="nav-wrapper">
       <ul className="main-nav-list">
-        <li><Link to="#">About</Link></li>
-        <li><Link to="#">Social</Link></li>
-        <li><Link to="#">Gallery</Link></li>
+        <li><Link to="#">about</Link></li>
+        <li><Link to="#">social</Link></li>
+        <li><Link to="#">gallery</Link></li>
       </ul>
     </nav>
     <section className="grid">
-    {/* <FlipCard/>
     <FlipCard/>
     <FlipCard/>
     <FlipCard/>
     <FlipCard/>
-    <FlipCard/> */}
+    <FlipCard/>
+    <FlipCard/>
   </section>
   </div>
 </div>
