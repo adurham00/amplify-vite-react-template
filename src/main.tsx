ffe0ec14aca9a922472bb32b5f2419bbb4c46fcb
adurham00 <apreece00@gmail.com>
2025-07-04 17:18:23 +0000

    attempt 100kabillion

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import "./index.css";
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from "aws-amplify";
-import outputs from "./amplify_outputs.json";
+import outputs from "../amplify_outputs.json";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
